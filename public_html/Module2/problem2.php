9b2099e9c84fa0a433b9395aa5bcc5fb2be64623
Jeremy De La Rosa <jeremyd26937@gmail.com>
2024-09-19 18:45:58 -0400

    problem 2 attempt

--- public_html/Module2/problem2.php
+++ public_html/Module2/problem2.php
@@ -9,8 +9,16 @@ function getTotal($arr) {
     //start edits
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO do adding here
+
+    //jd755 9-19-24
+    foreach($arr as $x)
+    {
+        $total += $x; 
+    }
     //TODO do rounding stuff here (round to two decimals i.e., 0.10, 0.01, 0.00)
-    
+    $total = $total*100;
+    round($total);
+    $total = $total/100;
     //end edits
     echo "The total is $total";
 }
